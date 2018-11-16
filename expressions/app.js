abd0e5fc8f92acd0d6461a5d66f6c4fcf8eff62b
ahmedaziz <ahmed83@me.com>
2018-11-16 14:00:18 +0100

    add expression folder

--- expressions/app.js
+++ expressions/app.js
@@ -7,12 +7,17 @@
     // MsgController.$inject = ['$scope'];
 
     function MsgController($scope) {
-      
+
       $scope.name = "Ahmed";
+      $scope.stateOfBeing = "hungry";
 
       $scope.sayMessage = function () {
         return "Ahmed loves food!"
-      }
-    }
+      };
+
+      $scope.feedAhmed = function () {
+        $scope.stateOfBeing = "feed";
+      };
+    };
 
 })();
